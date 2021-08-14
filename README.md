5fdb6a168c95f22b0fc6ba2839260b503dd38896
Zzzyl <2454398382@qq.com>
2021-08-14 18:09:43 +0800

    新增云函数上报

--- README.md
+++ README.md
@@ -1,8 +1,50 @@
 # ctgu_yiqing_baopingan
+新增云函数上报！！！
 
-自用小范围自动报平安，可从xlsx文件读取账号密码数据批量上报。
 
-目前有两种途径上报，一种是使用selenium模块模拟浏览器来上报，一种是使用requests模块操纵数据上报。选择其一就行。
+
+### 使用云函数部署
+
+使用腾讯云函数可每天自动上报，每天可以收到上报通知。
+
+#### 使用方法：
+1. 点此[注册腾讯云函数](https://console.cloud.tencent.com/)。
+2. 点此[新建云函数](https://console.cloud.tencent.com/scf/list-create?rid=4&ns=default&keyword=helloworld%20%E7%A9%BA%E7%99%BD%E6%A8%A1%E6%9D%BF%E5%87%BD%E6%95%B0%26python3)，配置都不用管。
+
+![image-20210814173933269](https://gitee.com/zzzjoy/My_Pictures/raw/master/image-20210814173933269.png)
+
+3. 配置云函数：
+   * 更改函数执行超时时间为900，不改也行，只是在日志里会报不通过，但是运行没问题。
+   
+     ![image-20210814174322964](https://gitee.com/zzzjoy/My_Pictures/raw/master/image-20210814174322964.png)
+
+   * 新建触发器，每天定时7点运行为cron表达式为0 0 7 * * *，建议自行查看文档设置定时任务。
+
+      ![image-20210814175714071](https://gitee.com/zzzjoy/My_Pictures/raw/master/image-20210814175714071.png)
+
+   * 部署函数代码，新建文件config.json配置文件，复制index.py内容和config.json内容，更改config文件内容，配置如下。其中消息推送渠道只有pushplus，请自行上[pushplus官网](https://pushplus.hxtrip.com/message)注册，获取token。
+
+      ```
+      config参数说明
+      pushplus_token(string)： pushplus推送渠道的token，自行去pushplus官网获取。
+      push_level(int)：通知等级，1-5级，分别是：
+          1. 每次都通知，通知详细信息。
+          2. 每次都通知，通知简略信息。
+          3. 报平安成功率!=100%时通知，通知详细信息。
+          4. 报平安成功率<90%时通知，通知详细信息。
+          5. 报平安成功率<50%时通知，通知详细信息。
+      timeout(double)：登录的超时时间，判断报平安服务器是否挂掉。
+      wait_time(double)：每报完一个的等待时间。
+      users(list)：用户列表，格式为[[usr1,pwd1,token1],[usr2,pwd2]]，其中每个用户的token为各自的pushplus_token，失败会推送消息到单个用户，可不填此参数。
+      ```
+
+      完成之后部署云函数。
+
+      ![image-20210814175110990](https://gitee.com/zzzjoy/My_Pictures/raw/master/image-20210814175110990.png)
+
+4. 测试
+
+   点击部署旁的测试即可，测试结果在日志管理中查看，测试成功后会收到pushplus推送消息
 
 ### 使用selenium模块：
 
@@ -28,4 +70,3 @@
 
 1. 安装依赖包：pip install -r requirements.txt
 2. 改info.xlsx的绝对路径
-
---
+++ baopingan_yunhanshu/index.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+import re
+import sys
+import logging
+import json
+import time
+import requests
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger()
+logger.setLevel(level=logging.INFO)
+
+
+# 读取配置
+def get_config():
+    with open("./config.json", 'r') as f:
+        load_dict = json.load(f)
+    return load_dict
+
+
+# 登录，为后续做准备
+def login(s, username, password, timeout, headers):
+    login_url = 'http://yiqing.ctgu.edu.cn/wx/index/loginSubmit.do'
+    data = {'username': username, 'password': password}
+    r = s.post(url=login_url, data=data, timeout=timeout, headers=headers)
+    if r.text == 'success':
+        return True
+
+
+# 退出登录
+def logout(s, headers):
+    logout_url = 'http://yiqing.ctgu.edu.cn/wx/index/logout.do'
+    s.get(url=logout_url, headers=headers)
+
+
+# 获取要带有data的html
+def get_student_info(s, headers):
+    student_info_url = 'http://yiqing.ctgu.edu.cn/wx/health/toApply.do'
+    r = s.get(url=student_info_url, headers=headers)
+    return r.text
+
+
+# 解析html,获得data数据
+def student_info_parse(html):
+    if len(re.findall(r'name="ttoken" value="(.*?)"/>', html)) == 0:
+        return False
+    data = {
+        'ttoken': re.findall(r'name="ttoken" value="(.*?)"/>', html)[0],  # ttoken
+        'province': re.findall(r'name="province" id="province" value="(.*?)"/>', html)[0],  # 省
+        'city': re.findall(r'name="city" id="city" value="(.*?)"/>', html)[0],  # 市
+        'district': re.findall(r'name="district" id="district" value="(.*?)"/>', html)[0],  # 县
+        'adcode': re.findall(r'name="adcode" id="adcode" value="(.*?)"/>', html)[0],  # 邮编
+        'longitude': re.findall(r'name="longitude" id="longitude" value="(.*?)"/>', html)[0],  # 经度
+        'latitude': re.findall(r'name="latitude" id="latitude" value="(.*?)"/>', html)[0],  # 纬度
+        'sfqz': '否',
+        'sfys': '否',
+        'sfzy': '否',
+        'sfgl': '否',
+        'status': re.findall(r'name="status" id="status" value="(.*?)"/>', html)[0],
+        'szdz': re.findall(r'name="szdz"  maxlength="20" value="(.*?)" readonly>', html)[0],  # 所在地区
+        'sjh': re.findall(r'name="sjh" maxlength="20" value="(.*?)">', html)[0],  # 手机号
+        'lxrxm': re.findall(r'name="lxrxm" maxlength="40" value="(.*?)"', html)[0],  # 紧急联系人姓名
+        'lxrsjh': re.findall(r'name="lxrsjh" maxlength="40" value="(.*?)"', html)[0],  # 紧急联系人电话
+        'sffr': '否',
+        'sffrAm': '否',
+        'sffrNoon': '否',
+        'sffrPm': '否',
+        'sffy': '否',
+        'sfgr': '否',
+        'qzglsj': '',
+        'qzgldd': '',
+        'glyy': '',
+        'mqzz': '',
+        'sffx': '否',
+        'qt': '',
+    }
+    return data
+
+
+# 向服务器post数据
+def sent_info(s, data, headers):
+    sent_info_url = 'http://yiqing.ctgu.edu.cn/wx/health/saveApply.do'
+    r = s.post(url=sent_info_url, data=data, headers=headers)
+    result = json.loads(r.text)
+    return result['msgStatus'] == 'true'
+
+
+# 推送消息
+def push_msg(msg, token):
+    pushplus_url = 'http://pushplus.hxtrip.com/send'
+    data = {
+        'token': token,
+        'title': '每日报平安通知',
+        'content': msg,
+        'template': 'html'
+    }
+    r = requests.post(url=pushplus_url, data=data)
+    result = json.loads(r.text)
+    if result['code'] == 200:
+        logger.info('消息发送成功')
+
+
+def main():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
+                  'application/signed-exchange;v=b3;q=0.9',
+    }
+
+    # 配置文件
+    config = get_config()
+    users = config['users']
+    timeout = config['timeout']
+    pushplus_token = config['pushplus_token']
+    push_level = config['push_level']
+    wait_time = config['wait_time']
+
+    total = len(users)
+    finished = 0
+    detail_msg = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'\n'
+    start_time = time.time()
+    for i in range(total):
+        # 报一个等待时间
+        time.sleep(wait_time)
+        s = requests.Session()
+        # 尝试登录
+        try:
+            flag = login(s, str(users[i][0]), str(users[i][1]), timeout, headers)
+        except requests.exceptions.RequestException as e:
+            logger.info(e)
+            s.close()
+            logger.info(str(users[i][0]) + ':登录超时')
+            detail_msg += str(users[i][0]) + ':登录超时\n'
+            if len(users[i]) == 3 and users[i][2] != '':
+                push_msg('自动上报平安失败，登录超时', users[i][2])
+            continue
+        # 登录成功
+        if flag:
+            # logger.info(str(users[i][0]) + ':登录成功')
+            html = get_student_info(s, headers)
+            data = student_info_parse(html)
+            # 今日已上报
+            if not data:
+                logout(s, headers)
+                s.close()
+                finished += 1
+                logger.info(str(users[i][0]) + ':今日已上报')
+                detail_msg += str(users[i][0]) + ':今日已上报\n'
+                continue
+            # 上报成功
+            if sent_info(s, data, headers):
+                logout(s, headers)
+                s.close()
+                logger.info(str(users[i][0]) + ':上报成功')
+                detail_msg += str(users[i][0]) + ':上报成功\n'
+                finished += 1
+            # 上报失败
+            else:
+                logout(s, headers)
+                s.close()
+                logger.info(str(users[i][0]) + ':上报失败')
+                detail_msg += str(users[i][0]) + ':上报失败\n'
+                if len(users[i]) == 3 and users[i][2] != '':
+                    push_msg('自动上报平安失败，服务异常', users[i][2])
+                continue
+        # 登录失败
+        else:
+            s.close()
+            logger.info(str(users[i][0]) + ':密码错误，登录失败')
+            detail_msg += str(users[i][0]) + ':密码错误\n'
+            if len(users[i]) == 3 and users[i][2] != '':
+                push_msg('自动上报平安失败，请检查密码', users[i][2])
+            continue
+
+    endtime = time.time()
+    usedtime = "%.3f" % (endtime - start_time)
+    nowtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    logger.info(nowtime+'  应报:'+str(total)+' 实报:'+str(finished)+' 用时:'+usedtime)
+
+    # 根据推送等级推送
+    if push_level == 1:
+        push_msg(detail_msg, pushplus_token)
+    elif push_level == 2:
+        push_msg(nowtime+'\n应报:'+str(total)+' 实报:'+str(finished)+' 用时:'+usedtime, pushplus_token)
+    elif push_level == 3:
+        if total != finished:
+            push_msg(detail_msg, pushplus_token)
+    elif push_level == 4:
+        if float(finished/total) < 0.9:
+            push_msg(detail_msg, pushplus_token)
+    elif push_level == 5:
+        if float(finished/total) < 0.5:
+            push_msg(detail_msg, pushplus_token)
+
+
+def main_handler(event, content):
+    main()
+
+
+main()
+
+
